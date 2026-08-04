2aa61b13523b601f54a803c8d11307ba246af9e4
JiSeong Lee <cocoquiet@knu.ac.kr>
2026-08-04 16:30:27 +0900

    Feat: Add duplicate checker method in repository(find_by_master_course_id_and_semester_id_and_major_id_and_section_number)

--- src/repository/course_repository.rs
+++ src/repository/course_repository.rs
@@ -54,6 +54,30 @@ impl CourseRepository {
             .first(conn)
     }
 
+    pub fn find_by_master_course_id_and_semester_id_and_major_id_and_section_number(
+        conn: &mut PgConnection,
+        master_course_id: i64,
+        semester_id: i64,
+        major_id: i64,
+        section_number: i32,
+    ) -> QueryResult<(Course, MasterCourse, Semester, Major)> {
+        course::table
+            .inner_join(master_course::table)
+            .inner_join(semester::table)
+            .inner_join(major::table)
+            .filter(course::master_course_id.eq(master_course_id))
+            .filter(course::semester_id.eq(semester_id))
+            .filter(course::major_id.eq(major_id))
+            .filter(course::section_number.eq(section_number))
+            .select((
+                Course::as_select(),
+                MasterCourse::as_select(),
+                Semester::as_select(),
+                Major::as_select(),
+            ))
+            .first(conn)
+    }
+
     pub fn update(
         conn: &mut PgConnection,
         course_id: i64,
