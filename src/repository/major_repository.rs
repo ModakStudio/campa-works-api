1fc4d9a1ea91603dde73ba208c85541d6458f5f8
JiSeong Lee <cocoquiet@knu.ac.kr>
2026-08-09 17:16:45 +0900

    Refactor: Adjust import boundery for visibility in repositories

--- src/repository/major_repository.rs
+++ src/repository/major_repository.rs
@@ -4,14 +4,14 @@ use diesel::prelude::*;
 
 use crate::{
     models::major::{Major, NewMajor, UpdateMajor},
-    schema::major::dsl::*,
+    schema::major,
 };
 
 pub struct MajorRepository;
 
 impl MajorRepository {
     pub fn create(conn: &mut PgConnection, new_major: &NewMajor) -> QueryResult<Major> {
-        diesel::insert_into(major)
+        diesel::insert_into(major::table)
             .values(new_major)
             .returning(Major::as_returning())
             .get_result(conn)
@@ -21,22 +21,22 @@ impl MajorRepository {
         conn: &mut PgConnection,
         params: &HashMap<String, String>,
     ) -> QueryResult<Vec<Major>> {
-        let mut query = major.select(Major::as_select()).into_boxed();
+        let mut query = major::table.select(Major::as_select()).into_boxed();
 
         if let Some(name_) = params
             .get("name")
             .map(|value| value.trim())
             .filter(|value| !value.is_empty())
         {
-            query = query.filter(name.ilike(format!("%{}%", name_)));
+            query = query.filter(major::name.ilike(format!("%{}%", name_)));
         }
 
         query.load(conn)
     }
 
     pub fn find_by_id(conn: &mut PgConnection, major_id: i64) -> QueryResult<Major> {
-        major
-            .filter(id.eq(major_id))
+        major::table
+            .filter(major::id.eq(major_id))
             .select(Major::as_select())
             .first(conn)
     }
@@ -46,13 +46,13 @@ impl MajorRepository {
         major_id: i64,
         update_major: &UpdateMajor,
     ) -> QueryResult<Major> {
-        diesel::update(major.filter(id.eq(major_id)))
+        diesel::update(major::table.filter(major::id.eq(major_id)))
             .set(update_major)
             .returning(Major::as_returning())
             .get_result(conn)
     }
 
     pub fn delete(conn: &mut PgConnection, major_id: i64) -> QueryResult<usize> {
-        diesel::delete(major.filter(id.eq(major_id))).execute(conn)
+        diesel::delete(major::table.filter(major::id.eq(major_id))).execute(conn)
     }
 }
--- src/repository/semester_repository.rs
+++ src/repository/semester_repository.rs
@@ -8,14 +8,14 @@ use crate::{
         enums::SemesterType,
         semester::{NewSemester, Semester, UpdateSemester},
     },
-    schema::semester::dsl::*,
+    schema::semester,
 };
 
 pub struct SemesterRepository;
 
 impl SemesterRepository {
     pub fn create(conn: &mut PgConnection, new_semester: &NewSemester) -> QueryResult<Semester> {
-        diesel::insert_into(semester)
+        diesel::insert_into(semester::table)
             .values(new_semester)
             .returning(Semester::as_returning())
             .get_result(conn)
@@ -25,34 +25,34 @@ impl SemesterRepository {
         conn: &mut PgConnection,
         params: &HashMap<String, String>,
     ) -> QueryResult<Vec<Semester>> {
-        let mut query = semester
+        let mut query = semester::table
             .select(Semester::as_select())
-            .order((year.desc(), semester_.asc()))
+            .order((semester::year.desc(), semester::semester_.asc()))
             .into_boxed();
 
         if let Some(semester_id) = params.get("id").and_then(|value| value.parse::<i64>().ok()) {
-            query = query.filter(id.eq(semester_id));
+            query = query.filter(semester::id.eq(semester_id));
         }
         if let Some(semester_year) = params
             .get("year")
             .and_then(|value| value.parse::<i32>().ok())
         {
-            query = query.filter(year.eq(semester_year));
+            query = query.filter(semester::year.eq(semester_year));
         }
         if let Some(semester_type) = params
             .get("semester_")
             .map(|value| value.trim())
             .filter(|value| !value.is_empty())
         {
-            query = query.filter(semester_.eq(SemesterType::from(semester_type)));
+            query = query.filter(semester::semester_.eq(SemesterType::from(semester_type)));
         }
 
         query.load(conn)
     }
 
     pub fn find_by_id(conn: &mut PgConnection, semester_id: i64) -> QueryResult<Semester> {
-        semester
-            .filter(id.eq(semester_id))
+        semester::table
+            .filter(semester::id.eq(semester_id))
             .select(Semester::as_select())
             .first(conn)
     }
@@ -62,13 +62,13 @@ impl SemesterRepository {
         semester_id: i64,
         update_semester: &UpdateSemester,
     ) -> QueryResult<Semester> {
-        diesel::update(semester.filter(id.eq(semester_id)))
+        diesel::update(semester::table.filter(semester::id.eq(semester_id)))
             .set(update_semester)
             .returning(Semester::as_returning())
             .get_result(conn)
     }
 
     pub fn delete(conn: &mut PgConnection, semester_id: i64) -> QueryResult<usize> {
-        diesel::delete(semester.filter(id.eq(semester_id))).execute(conn)
+        diesel::delete(semester::table.filter(semester::id.eq(semester_id))).execute(conn)
     }
 }
